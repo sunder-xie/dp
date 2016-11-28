176dbc10518a0b637f08d93d405f500518e7ec6b
黄章挺 <zhangting.huang@tqmall.com>
2016-11-28 20:33:47 +0800

    搜狐车型数据匹配

--- main/src/main/java/crawl/sohu/HandleCarTest.java
+++ main/src/main/java/crawl/sohu/HandleCarTest.java
@@ -3,7 +3,6 @@ package crawl.sohu;
 import base.BaseTest;
 import dp.common.util.*;
 import dp.common.util.excelutil.CommReaderXLS;
-import dp.common.util.excelutil.CommReaderXLSX;
 import org.junit.Test;
 
 import java.util.*;
@@ -14,6 +13,8 @@ import java.util.*;
 public class HandleCarTest extends BaseTest {
     private Map<String, String> brandMap;
     private Set<String> spBrandSet;
+    private List<Map<String, Object>> unMatchCarList;
+    private List<Map<String, Object>> matchCarList;
 
 
     public List<Map<String, Object>> getCarList(){
@@ -35,6 +36,7 @@ public class HandleCarTest extends BaseTest {
         return commonMapper.selectListBySql(sql);
     }
 
+    //匹配车型
     @Test
     public void carTest() throws Exception{
         List<Map<String, Object>> carList = getAllCarList();
@@ -64,9 +66,18 @@ public class HandleCarTest extends BaseTest {
 
         init();
 
-        for(Map<String, String> shCar : shCarList){
-            compareCar(shCar, carList);
+        for(Map<String, Object> car : carList){
+            compareCar(car, shCarList, 1);
         }
+        Print.info("匹配上的车型："+matchCarList.size());
+        Print.info("第一次没匹配上的车型："+unMatchCarList.size());
+
+        List<Map<String, Object>> unMatchList = new ArrayList<>(unMatchCarList);
+        unMatchCarList = new ArrayList<>();
+        for(Map<String, Object> umCar : unMatchList){
+            compareCar(umCar, shCarList, 2);
+        }
+        Print.info("匹配上的车型："+matchCarList.size());
 
         IoUtil.closeWriter(writer);
     }
@@ -74,6 +85,8 @@ public class HandleCarTest extends BaseTest {
     public void init(){
         brandMap = initBrandMap();
         spBrandSet = initSpBrandSet();
+        unMatchCarList = new ArrayList<>();
+        matchCarList = new ArrayList<>();
 
         String dateStr = DateUtils.dateToString(new Date(), DateUtils.yyyyMMdd);
         String sqlFile = path + "addCarRelation-"+dateStr+".sql";
@@ -103,64 +116,66 @@ public class HandleCarTest extends BaseTest {
         return set;
     }
 
-    public void compareCar(Map<String, String> shCar, List<Map<String, Object>> carList){
-        String shBrand = handleBrand(StrUtil.toUpCase(shCar.get("brand")));
-        if(spBrandSet.contains(shBrand)){
-            compareCarSp(shCar, carList);
-            return;
-        }
-        String name = shCar.get("name");
-
-        Set<String> matchCarIds = new HashSet<>();
-        for(Map<String, Object> car : carList){
-            if(shBrand.equals(car.get("brand").toString()) && compareModel(shCar, car)){
-                if(shCar.get("year").equals(car.get("year").toString())
-                        && comparePower(car, name)){
-
-                    matchCarIds.add(car.get("id").toString());
+    public void compareCar(Map<String, Object> car, List<Map<String, String>> shCarList, int compareTime){
+        String brand = car.get("brand").toString();
+        String year = car.get("year").toString();
+        String name = car.get("name").toString();
+        String shCarId = null;
+        for(Map<String, String> shCar : shCarList){
+            String shBrand = handleBrand(StrUtil.toUpCase(shCar.get("brand")));
+            String shName = shCar.get("name");
+            if(brand.equals(shBrand) && comparePower(car, shName)
+                    && year.equals(shCar.get("year")) && compareTransmissionType(name, shName)){
+                if(spBrandSet.contains(shBrand)){
+                    if(compareModelSp(shCar, car)){
+                        shCarId = shCar.get("id");
+                        break;
+                    }
+                }else{
+                    if(compareModel(shCar, car, compareTime)){
+                        shCarId = shCar.get("id");
+                        break;
+                    }
                 }
             }
         }
-        if(matchCarIds.isEmpty()){
-            //Print.info("没有匹配上的："+shCar);
+
+        //如果没有匹配上
+        if(shCarId==null){
+            unMatchCarList.add(car);
         }else{
-            //Print.info(shCar.get("id") + "  " + matchCarIds);
-            writeSql(shCar.get("id"), matchCarIds);
+            car.put("shCarId", shCarId);
+            matchCarList.add(car);
         }
     }
 
-    private boolean comparePower(Map<String, Object> car, String saleName){
-        String power = car.get("power").toString().replace("T", "").replace("L", "");
-        if(saleName.contains(power)){
-            return !(saleName.contains("柴油") ^ car.get("name").toString().contains("柴油"));
-        }
-        return false;
-    }
-
-    public boolean compareModel(Map<String, String> shCar, Map<String, Object> car){
+    public boolean compareModel(Map<String, String> shCar, Map<String, Object> car, int compareTime){
         String importInfo = car.get("import_info").toString();
         String shCompany = shCar.get("company");
         if("进口".equals(importInfo) ^ shCompany.contains("进口")){
             return false;
         }
-
         String shModel = StrUtil.toUpCase(shCar.get("model"));
         String model = StrUtil.toUpCase(car.get("model").toString());
-        if(shModel.equals(model)){
-            return true;
-        }
 
-        if(shModel.contains(model) || model.contains(shModel)){
-            return true;
+        if(1==compareTime) {
+            if (shModel.equals(model)) {
+                return true;
+            }
+            shModel = shModel.replace("双门", "COUPE").replace("掀背", "SPORTBACK");
+            return shModel.equals(model);
+        }else{
+            if(shModel.contains(model) || model.contains(shModel)){
+                return true;
+            }
+            model = StrUtil.rep(model);
+            if(shModel.contains(model) || model.contains(shModel)){
+                return true;
+            }
+            shModel = shModel.replace("双门", "COUPE").replace("掀背", "SPORTBACK");
+            return shModel.contains(model) || model.contains(shModel);
         }
-        model = StrUtil.rep(model);
-        if(shModel.contains(model) || model.contains(shModel)){
-            return true;
-        }
-        shModel = shModel.replace("双门", "COUPE").replace("掀背", "SPORTBACK");
-        return shModel.contains(model) || model.contains(shModel);
     }
-
 
     public boolean compareModelSp(Map<String, String> shCar, Map<String, Object> car){
         String importInfo = car.get("import_info").toString();
@@ -183,28 +198,32 @@ public class HandleCarTest extends BaseTest {
         return false;
     }
 
-    //处理奔驰、宝马
-    public void compareCarSp(Map<String, String> shCar, List<Map<String, Object>> carList){
-        String shBrand = shCar.get("brand");
-        String shCompany = shCar.get("company");
-        String name = shCar.get("name");
-
-        Set<String> matchCarIds = new HashSet<>();
-        for(Map<String, Object> car : carList){
-            if(shBrand.equals(car.get("brand").toString()) && compareModelSp(shCar, car)){
-                if(shCar.get("year").equals(car.get("year")) &&
-                        comparePower(car, name)){
-
-                    matchCarIds.add(car.get("id").toString());
-                }
-            }
+    private boolean comparePower(Map<String, Object> car, String saleName){
+        String power = car.get("power").toString().replace("T", "").replace("L", "");
+        if(saleName.contains(power)){
+            return !(saleName.contains("柴油") ^ car.get("name").toString().contains("柴油"));
         }
-        if(matchCarIds.isEmpty()){
-            //Print.info("sp没有匹配上的："+shCar);
-        }else{
-            //Print.info(shCar.get("id") + "  " + matchCarIds);
-            writeSql(shCar.get("id"), matchCarIds);
+        return false;
+    }
+
+    public boolean compareTransmissionType(String tqCarName, String shCarName){
+        String tqName = StrUtil.toUpCase(tqCarName);
+        String shName = StrUtil.toUpCase(shCarName);
+
+        if(shName.contains("DSG")){
+            return tqName.contains("双离合");
         }
+        if(shName.contains("CVT")){
+            return tqName.contains("无级");
+        }
+        if(shName.contains("手自一体") || shName.contains("自动") || shName.contains("AT")){
+            return tqName.contains("自动");
+        }
+        if(shName.contains("手动") || shName.contains("MT")){
+            return tqName.contains("手动");
+        }
+
+        return true;
     }
 
     public String handleBrand(String shBrand){
@@ -224,59 +243,6 @@ public class HandleCarTest extends BaseTest {
             IoUtil.writeFile(writer, sb.toString());
             sb.setLength(0);
         }
-    }
-
-
-    //临时处理
-    public void compareBrand(List<Map<String, String>> shCarList, List<Map<String, Object>> carList){
-        Set<String> set = new HashSet<>();
-        for(Map<String, String> shCar : shCarList){
-            String shBrand = handleBrand(StrUtil.toUpCase(shCar.get("brand")));
-            boolean flag = true;
-            for(Map<String, Object> car : carList){
-                if(shBrand.equals(StrUtil.toUpCase(car.get("brand").toString()))){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                set.add(shBrand);
-            }
-        }
-
-        Print.info(set);
-    }
-
-
-    //处理需要删除的对应关系
-    @Test
-    public void testDel() throws Exception{
-        path = "/Users/huangzhangting/Documents/数据处理/爬取数据/搜狐汽车/";
-        String excel = path + "需要删除的关系.xlsx";
-
-        Map<String, String> attrMap = new HashMap<>();
-        attrMap.put("t1.id", "sh_car_id");
-        attrMap.put("t3.id", "tq_car_id");
-
-        CommReaderXLSX readerXLSX = new CommReaderXLSX(attrMap, Constant.TYPE_LIST, 0);
-        readerXLSX.processOneSheet(excel, 1);
-        Print.info(readerXLSX.getDataList().size());
-
-        String dateStr = DateUtils.dateToString(new Date(), DateUtils.yyyyMMdd);
-        writer = IoUtil.getWriter(path+"delCarRelation-"+dateStr+".sql");
-
-        StringBuilder sb = new StringBuilder();
-        for(Map<String, String> data : readerXLSX.getDataList()){
-            sb.append("delete from sohu_car_relation where sh_car_id=");
-            sb.append(data.get("sh_car_id"));
-            sb.append(" and tq_car_id=").append(data.get("tq_car_id"));
-            sb.append(";\n");
-
-            IoUtil.writeFile(writer, sb.toString());
-            sb.setLength(0);
-        }
-
-        IoUtil.closeWriter(writer);
     }
 
 }
