c321e8500e65009ed6fa1f56c30a8eb45f138650
黄章挺 <zhangting.huang@tqmall.com>
2017-04-12 11:19:58 +0800

    mybatis generator plugin 开发

--- mybatis-generator/src/main/java/tqmall/mybatis/generator/PluginTest.java
+++ mybatis-generator/src/main/java/tqmall/mybatis/generator/PluginTest.java
@@ -3,10 +3,16 @@ package tqmall.mybatis.generator;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.OutputUtilities;
 import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.codegen.mybatis3.ListUtilities;
+import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,6 +22,9 @@ import java.util.Set;
  */
 public class PluginTest extends PluginAdapter {
     private FullyQualifiedJavaType lombokData;
+    private final String batchInsert = "batchInsert";
+    private final String batchInsertList = "list";
+
 
     public PluginTest() {
         super();
@@ -81,15 +90,14 @@ public class PluginTest extends PluginAdapter {
         generateBatchInsertMethod(interfaze, introspectedTable);
         return true;
     }
-
     /** 生成批量插入的方法 **/
     private void generateBatchInsertMethod(Interface interfaze, IntrospectedTable introspectedTable){
-        Method method = new Method("batchInsert");
+        Method method = new Method(batchInsert);
         method.setReturnType(FullyQualifiedJavaType.getIntInstance());
 
         FullyQualifiedJavaType listType = FullyQualifiedJavaType.getNewListInstance();
         listType.addTypeArgument(new FullyQualifiedJavaType(introspectedTable.getBaseRecordType()));
-        Parameter parameter = new Parameter(listType, "list");
+        Parameter parameter = new Parameter(listType, batchInsertList);
         method.addParameter(parameter);
 
         method.addJavaDocLine("/** 批量插入 **/");
@@ -100,9 +108,70 @@ public class PluginTest extends PluginAdapter {
 
     @Override
     public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
-        XmlElement rootElement = document.getRootElement();
-        XmlElement insert = new XmlElement("insert");
-
+        generateBatchInsertSql(document, introspectedTable);
         return true;
     }
+    /** 生成批量插入sql **/
+    private void generateBatchInsertSql(Document document, IntrospectedTable introspectedTable){
+        XmlElement rootElement = document.getRootElement();
+        XmlElement insert = new XmlElement("insert");
+        insert.addAttribute(new Attribute("id", batchInsert));
+        FullyQualifiedJavaType parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+        insert.addAttribute(new Attribute("parameterType", parameterType.getFullyQualifiedName()));
+
+        StringBuilder insertClause = new StringBuilder();
+        StringBuilder valuesClause = new StringBuilder();
+
+        insertClause.append("insert into ");
+        insertClause.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+        insertClause.append(" (");
+        insert.addElement(new TextElement(insertClause.toString()));
+        insertClause.setLength(0);
+        OutputUtilities.xmlIndent(insertClause, 1); //缩进
+
+        String foreachItem = "item";
+        List<String> valuesClauseList = new ArrayList<String>();
+        List<IntrospectedColumn> columnList = ListUtilities.removeIdentityAndGeneratedAlwaysColumns(introspectedTable.getAllColumns());
+        int size = columnList.size();
+        for(int i=0; i<size; i++){
+            IntrospectedColumn introspectedColumn = columnList.get(i);
+            insertClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            valuesClause.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn, foreachItem+"."));
+            if (i + 1 < size) {
+                insertClause.append(", ");
+                valuesClause.append(", ");
+            }
+            if(valuesClause.length() > 80){ //避免单行字符过长
+                insert.addElement(new TextElement(insertClause.toString())); //insert语句，直接添加
+                insertClause.setLength(0);
+                OutputUtilities.xmlIndent(insertClause, 1); //缩进
+
+                valuesClauseList.add(valuesClause.toString());
+                valuesClause.setLength(0);
+//                OutputUtilities.xmlIndent(valuesClause, 1);
+            }
+        }
+        //因为每行 >80 就直接添加了，所以还剩最后一行数据
+        valuesClauseList.add(valuesClause.toString());
+        insertClause.append(")");
+        insert.addElement(new TextElement(insertClause.toString()));
+
+        insert.addElement(new TextElement("values"));
+
+        XmlElement foreach = new XmlElement("foreach");
+        foreach.addAttribute(new Attribute("collection", batchInsertList));
+        foreach.addAttribute(new Attribute("item", foreachItem));
+        foreach.addAttribute(new Attribute("index", "index"));
+        foreach.addAttribute(new Attribute("separator", ","));
+        foreach.addElement(new TextElement("("));
+        for(String val : valuesClauseList){
+            foreach.addElement(new TextElement(val));
+        }
+        foreach.addElement(new TextElement(")"));
+
+        insert.addElement(foreach);
+
+        rootElement.addElement(insert);
+    }
+
 }
