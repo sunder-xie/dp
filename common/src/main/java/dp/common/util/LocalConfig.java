a76eb01555837a5f711f2a3d5d187bc52bdd171f
huangzhangting <15158036445@163.com>
2017-05-24 19:24:33 +0800

    本地配置

---
+++ common/src/main/java/dp/common/util/LocalConfig.java
@@ -0,0 +1,16 @@
+package dp.common.util;
+
+/**
+ * Created by huangzhangting on 2017/5/24.
+ */
+public class LocalConfig {
+    public static final String HAO_SERVICE_APP_KEY = "";
+    public static final String JI_SU_APP_KEY = "";
+    public static final String JU_HE_APP_KEY = "";
+
+    public static final String MANA_ENCRYPT_KEY = "";
+    public static final String UPYUN_BUCKET_NAME = "";
+    public static final String UPYUN_OPERATOR_NAME = "";
+    public static final String UPYUN_OPERATOR_PWD = "";
+
+}
